2850fbdfc5c030979bde54f623f4b5c979f27656
winstoncwang <congwang@live.co.uk>
2020-06-10 16:17:03 +0100

    V1.3
    adding different route for different pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={StreamList} />
+					<Route
+						path="/streams/create"
+						exact
+						component={StreamCreate}
+					/>
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route
+						path="/streams/delete"
+						exact
+						component={StreamDelete}
+					/>
+					<Route path="/streams/show" exact component={StreamShow} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
