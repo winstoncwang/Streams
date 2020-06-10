36e73526227a9df09c4773b9fd4e4f2a0f09ecf7
winstoncwang <congwang@live.co.uk>
2020-06-10 16:29:01 +0100

    V1.4
    add header and fix link outisde browserrouter error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,12 +6,14 @@ import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
+import Header from './Header';
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
 			<BrowserRouter>
 				<div>
+					<Header />
 					<Route path="/" exact component={StreamList} />
 					<Route
 						path="/streams/create"
