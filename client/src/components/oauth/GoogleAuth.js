0a1f56afa85083cc1091235c083fe3b072f12a82
winstoncwang <congwang@live.co.uk>
2020-06-14 14:15:44 +0100

    V2.2
    load and init the oauth process

--- client/src/components/oauth/GoogleAuth.js
+++ client/src/components/oauth/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+	componentDidMount () {
+		window.gapi.load('client:auth2', () => {
+			window.gapi.init({
+				clientId :
+					'243290141274-g02ba5te26jsmvbq5mhgbh65q5i8n0qk.apps.googleusercontent.com',
+				scope    : 'email'
+			});
+		});
+	}
+
 	render () {
 		return <div>GoogleAuth</div>;
 	}
