518f25685dffdf0e6bea57664b23761224ded819
winstoncwang <congwang@live.co.uk>
2020-06-14 14:54:57 +0100

    V2.3
    fix small bug with syntax error and using auth object to determine the login states

--- client/src/components/oauth/GoogleAuth.js
+++ client/src/components/oauth/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+	state = { isSignedIn: null };
+
 	componentDidMount () {
 		window.gapi.load('client:auth2', () => {
-			window.gapi.init({
-				clientId :
-					'243290141274-g02ba5te26jsmvbq5mhgbh65q5i8n0qk.apps.googleusercontent.com',
-				scope    : 'email'
-			});
+			window.gapi.client
+				.init({
+					clientId :
+						'243290141274-g02ba5te26jsmvbq5mhgbh65q5i8n0qk.apps.googleusercontent.com',
+					scope    : 'email'
+				})
+				.then(() => {
+					this.auth = window.gapi.auth2.getAuthInstance();
+					this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+				});
 		});
 	}
 
+	renderAuthButton () {
+		if (this.state.isSignedIn === null) {
+			return <div>Signed in state unknown</div>;
+		} else if (this.state.isSignedIn) {
+			return <div>I am signed in</div>;
+		} else {
+			return <div>I am not signed in</div>;
+		}
+	}
+
 	render () {
-		return <div>GoogleAuth</div>;
+		return <div>{this.renderAuthButton()}</div>;
 	}
 }
 
