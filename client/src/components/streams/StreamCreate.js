5fd75649f931faac46c281fec8bf08a3d816e8a4
winstoncwang <congwang@live.co.uk>
2020-06-20 21:34:22 +0100

    V3.2
    adding redux form, using field component to render input box

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,33 @@
 import React from 'react';
+//Field is a component, reduxForm is a function similar to connect() in react-redux
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-	return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+	renderInput (formProps) {
+		return (
+			<div className="field">
+				<label>{formProps.label}</label>
+				<input {...formProps.input} />
+			</div>
+		);
+	}
 
-export default StreamCreate;
+	render () {
+		return (
+			<form className="ui form">
+				<Field
+					name="title"
+					component={this.renderInput}
+					label="Enter Title"
+				/>
+				<Field
+					name="description"
+					component={this.renderInput}
+					label="Enter Description"
+				/>
+			</form>
+		);
+	}
+}
+
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);
