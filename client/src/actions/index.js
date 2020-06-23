6300682fe5bd97806530282b353f47c43ee420b5
winstoncwang <51161138+winstoncwang@users.noreply.github.com>
2020-06-23 18:29:12 +0100

    V4.3
    batch creating action creator using restful conventions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,50 @@
 import streams from '../api/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	FETCH_STREAMS,
+	FETCH_STREAM,
+	DELETE_STREAM,
+	EDIT_STREAM
+} from './types';
 
 export const createStream = (formValues) => {
 	return async (dispatch) => {
 		const response = await streams.post('/streams', formValues);
+		dispatch({ type: CREATE_STREAM, payload: response.data });
+	};
+};
+//get a list of streamss
+export const fetchStreams = () => {
+	return async (dispatch) => {
+		const response = await streams.get('/streams');
+
+		dispatch({ type: FETCH_STREAMS, payload: response.data });
+	};
+};
+
+export const editStream = (id, formValues) => {
+	return async (dispatch) => {
+		const response = await streams.put(`/streams/${id}`, formValues);
+
+		dispatch({ type: EDIT_STREAM, payload: response.data });
+	};
+};
+
+export const fetchStream = (id) => {
+	return async (dispatch) => {
+		const response = await streams.get(`/streams/${ids}`);
+
+		dispatch({ type: FETCH_STREAM, payload: response.data });
+	};
+};
+
+export const deleteStream = (id) => {
+	return async (dispatch) => {
+		const response = await streams.delete(`/streams/${id}`);
+
+		dispatch({ type: DELETE_STREAM, payload: response.data });
 	};
 };
 
